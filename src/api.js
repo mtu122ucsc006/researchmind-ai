a26f7fbeefd6ed9252039e72a2aea2fab128eab8
harini <harinipathmanaban@gmail.com>
2026-06-23 23:08:32 +0530

    fix api url for vercel

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
@@ -9,13 +9,13 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('researchmind_token')
+
   if (token) {
     config.headers = config.headers || {}
     config.headers.Authorization = `Bearer ${token}`
   }
 
   if (config.data instanceof FormData) {
-    // Let Axios set the multipart boundary automatically.
     delete config.headers['Content-Type']
   }
 
